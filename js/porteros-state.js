d017bb3b710e5bb5a10ac1f93305d879d4ed8a93
PabliVM <pablo.vegamartin@gmail.com>
2026-05-25 13:35:50 +0200

    Update porteros-state.js

--- js/porteros-state.js
+++ js/porteros-state.js
@@ -1,35 +1,6 @@
-export const PORTEROS_TEAMS = [
-  { key: "JA", name: "Juvenil A" },
-  { key: "JB", name: "Juvenil B" },
-  { key: "JC", name: "Juvenil C" },
-  { key: "CA", name: "Cadete A" },
-  { key: "CB", name: "Cadete B" },
-  { key: "IA", name: "Infantil A" },
-  { key: "IB", name: "Infantil B" },
-  { key: "AA", name: "Alevín A" },
-  { key: "AB", name: "Alevín B" },
-  { key: "BA", name: "Benjamín A" },
-  { key: "BB", name: "Benjamín B" }
-];
-
-export const DAY_TYPES = [
-  "Entrenamiento",
-  "Partido",
-  "Descanso",
-  "Torneo",
-  "Libre"
-];
-
-export const TRAINING_BLOCKS = [
-  "Preparación Física",
-  "Entrenamiento en Campo",
-  "Video análisis",
-  "Informe contenidos micro"
-];
-
-export const PORTEROS_ICONS = {
-  preparacionFisica: "./assets/icons/brazo.png",
-  entrenamientoCampo: "./assets/icons/balon.png",
-  videoAnalisis: "./assets/icons/video.png",
-  informeMicro: "./assets/icons/informe.png"
+export const porterosState = {
+  activeTeam: "JA",
+  activeMonday: null,
+  activeWeekDays: [],
+  activeDayPlans: []
 };
